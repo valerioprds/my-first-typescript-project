9ba68e209cb6fd5301135a7bf572539f7cb41f31
ValerioParadiso <v.paradiso89@gmail.com>
2024-06-06 18:51:53 +0200

    added substract implementation closes #1

--- test/math.spec.ts
+++ test/math.spec.ts
@@ -1,6 +1,10 @@
- import { add } from "../src/math";
+ import { add, substract } from "../src/math";
 describe("Math functions", () => {
 	it("should add two numbers correctly", () => {
 		expect(add(1, 2)).toEqual(3);
+	});
+
+	it("should subtract two numbers correctly", () => {
+		expect(substract(1, 2)).toEqual(3);
 	});
 });
